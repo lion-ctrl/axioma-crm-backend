0bfa8e7f841294d2b8ab858b445d2f990d0af9ac
Leonardo <leonardo.novaro.ln@gmail.com>
2021-03-17 19:42:10 -0400

    Creando modelo de Usuarios

---
+++ src/models/Usuarios.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const UsuariosSchema = new Schema({
+	nombre: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	apellido: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	creado: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+module.exports = model("usuarios", UsuariosSchema);
