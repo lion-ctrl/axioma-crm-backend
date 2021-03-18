69d2fecc84135e7741be7704dd518d125ef0a74a
Leonardo <leonardo.novaro.ln@gmail.com>
2021-03-17 20:43:39 -0400

    Codigo para crear usuario, crear un usuario y regresar jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,45 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+require("dotenv").config({path:"variables.env"})
 
+const Usuarios = require("../models/Usuarios");
 
-exports.autenticarUsuario = (req,res) => {
-}
+exports.autenticarUsuario = (req, res) => {};
 
-exports.obtenerUsuario = (req,res) => {
-}
+exports.obtenerUsuario = (req, res) => {};
 
-exports.crearUsuario = (req,res) => {
+exports.validarNuevoUsuario = [
+	check("nombre", "El nombre es obligatorio").notEmpty(),
+	check("email", "Agrega un email valido").isEmail(),
+	check("password", "La contraseña debe ser de minimo 6 caracteres").isLength({
+		min: 6,
+	}),
+	function (req, res, next) {
+		const errores = validationResult(req);
+		if (!errores.isEmpty()) {
+			return res.status(400).json({ errores: errores.array() });
+		}
+		next();
+	},
+];
 
-}
+exports.crearUsuario = async (req, res) => {
+	try {
+		let usuario = await Usuarios.findOne({ email: req.body.email });
+		if (usuario) return res.status(400).json({ msg: "Ese correo ya existe" });
+
+		usuario = new Usuarios(req.body);
+
+		const salt = await bcrypt.genSalt(10);
+		usuario.password = await bcrypt.hash(req.body.password, salt);
+		await usuario.save();
+
+		// JWT
+		const token = jwt.sign({ _id: usuario._id }, process.env.SECRETA, { expiresIn: "24h" });
+        return res.status(200).json({token})
+	} catch (error) {
+		console.log(error);
+        res.status(400).json({msg: "Hubo un Error"});
+	}
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,10 @@ const authController = require("../controllers/authController");
 router
 	.get("/", authController.obtenerUsuario)
 	.post("/", authController.autenticarUsuario)
-	.post("/nuevo-usuario", authController.crearUsuario);
+	.post(
+		"/nuevo-usuario",
+		authController.validarNuevoUsuario,
+		authController.crearUsuario
+	);
 
 module.exports = router;
